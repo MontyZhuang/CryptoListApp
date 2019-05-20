42e91ba7b9f23c1d83d2d6715ef61e1daee6180f
Monty <montyzhuang@gmail.com>
2019-05-20 16:01:08 +0100

    Added portfolio activity

---
+++ app/src/main/java/com/stickurr/cmc2/PortfolioActivity.java
@@ -0,0 +1,15 @@
+package com.stickurr.cmc2;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PortfolioActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_portfolio);
+    }
+}
